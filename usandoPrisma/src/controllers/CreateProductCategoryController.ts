2bb7eb4d029ca2faf6efeee3886cb528657a57b8
CleberSalustiano <kleberdejesus45@hotmail.com>
2022-09-15 23:31:31 -0300

    feat: using prisma orm to do connect with database

---
+++ usandoPrisma/src/controllers/CreateProductCategoryController.ts
@@ -0,0 +1,14 @@
+import {Request, Response} from "express"
+import { prismaClient } from "../database/prismaClient";
+
+export class CreateProductCategoryController {
+  async handle (request: Request, response: Response) {
+    const { id_product, id_category } = request.body;
+
+    const productCategory = await prismaClient.producttCategory.create({
+      data: {id_category, id_product}
+    })
+
+    return response.json(productCategory);
+  }
+}
---
+++ usandoPrisma/src/routes.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { CreateCategoryController } from "./controllers/CreateCategoryController";
+import { CreateProductCategoryController } from "./controllers/CreateProductCategoryController";
+import { CreateProductController } from "./controllers/CreateProductController";
+import { CreateProductWithExistCategory } from "./controllers/CreateProductWithExistCategory";
+
+const router = Router();
+
+const createProduct = new CreateProductController();
+const createCategory = new CreateCategoryController();
+const createProductCategory = new CreateProductCategoryController();
+const createProductWithExistCategory = new CreateProductWithExistCategory();
+
+router.post("/product", createProduct.handle);
+router.post("/category", createCategory.handle);
+router.post("/categoryProduct", createProductCategory.handle);
+router.post("/categoryWithExists", createProductWithExistCategory.handle);
+
+
+export {router};
